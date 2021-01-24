72b6bc8c72761fb7493bafb355159442f355eb42
hamotskyi <a.plus.t.17.8.infinity@gmail.com>
2021-01-25 01:09:52 +0200

    first slider 50% done

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.headerSlider').slick({
+        infinite: true,
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+  });
